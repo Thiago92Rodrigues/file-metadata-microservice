f7b14c7882f65ade144af15472fea0d7f0b7ee73
Thiago Rodrigues <45070690+Thiago92Rodrigues@users.noreply.github.com>
2019-11-02 16:31:28 -0300

    add file upload endpoint

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
 'use strict';
 
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
 
-// require and use "multer"...
-
-var app = express();
+const app = express();
+const upload = multer();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+  res.sendFile(process.cwd() + '/views/index.html');
+});
 
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+// file upload endpoint
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  res.json({
+    "name" : req.file.originalname,
+    "size" : req.file.size
+  });
+});
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
